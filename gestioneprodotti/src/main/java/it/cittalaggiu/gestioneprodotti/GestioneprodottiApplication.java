96523bdc82434e1d4f2ce225226ee8de20ed2452
Federico-Massaccesi <fede99ita26@gmail.com>
2024-09-04 12:08:16 +0200

    first commit

---
+++ gestioneprodotti/src/main/java/it/cittalaggiu/gestioneprodotti/GestioneprodottiApplication.java
@@ -0,0 +1,13 @@
+package it.cittalaggiu.gestioneprodotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneprodottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneprodottiApplication.class, args);
+	}
+
+}
---
+++ gestioneprodotti/src/test/java/it/cittalaggiu/gestioneprodotti/GestioneprodottiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cittalaggiu.gestioneprodotti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneprodottiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
